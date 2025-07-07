efaf0ca34a2d3df3f23b672ca2a35f026b3065bb
Chimipupu <chiminyanyan@gmail.com>
2025-07-07 23:45:23 +0900

    10カウント回路を修正

--- ip_pj/cnt_10/cnt_10.v
+++ ip_pj/cnt_10/cnt_10.v
@@ -2,20 +2,18 @@
 // Ver: 0.1 (2025/7/7)
 // Copyright (c) 2025 Chimipupu(https://github.com/Chimipupu) All Rights Reserved.
 
-module counter(CLK, RES, Q);
+module counter(
+        input CLK,
+        input RES,
+        output reg [3:0] Q
+    );
 
-input CLK, RES;
-output [3:0]Q;
-
-wire CLK, RES;
-reg [3:0]Q;
-
-always @(posedge CLK or negedge RES)
-    if(RES ==1'b0)
-        Q = 4'd0;
-    else if(Q == 4'd9)
-        Q = 4'd0;
-    else
-        Q = Q + 4'd1;
+    always @(posedge CLK or negedge RES) begin
+        // リセット or 4bitの最大9 = 出力０
+        if (RES == 1'b0 || Q == 4'd9)
+            Q <= 4'd0;
+        else
+            Q <= Q + 4'd1;
+    end
 
 endmodule
--- ip_pj/cnt_10/cnt_10_tb.v
+++ ip_pj/cnt_10/cnt_10_tb.v
@@ -3,25 +3,28 @@
 // Copyright (c) 2025 Chimipupu(https://github.com/Chimipupu) All Rights Reserved.
 
 module counter_test;
+    reg CLK, RES;
+    wire [3:0] Q;
 
-reg CLK, RES;
-wire [3:0] Q;
+    // インスタンス化: counterモジュール (CLK, RES, Q)
+    counter i0(CLK, RES, Q);
 
-counter i0(CLK, RES, Q);
+    // クロック信号の生成: 5nsごとに反転 → 10ns周期 (100MHz)
+    always #5 CLK = ~CLK;
 
-always #5 CLK = ~CLK;
+    // 初期化とリセット制御
+    initial begin
+        CLK = 0;    // クロック初期値
+        RES = 0;    // リセット初期値 (非アサート)
+        #23 RES = 1; // 23ns後にリセットをアサート
+        #150 $finish; // 150ns後にシミュレーション終了
+    end
 
-initial begin
-        CLK = 0;
-        RES = 0;
-#23     RES = 1;
-#150    $finish;
-end
-
-initial begin
+    // シミュレーション出力の設定
+    initial begin
         $monitor("CLK=%d, RES=%d, Q=%d", CLK, RES, Q);
         $dumpfile("counter.vcd");
         $dumpvars(0, counter_test);
-end
+    end
 
 endmodule
